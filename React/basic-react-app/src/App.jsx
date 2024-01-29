739adcabf41f252c5845b594a7dc7ed67e7fa5c9
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-01-29 06:45:50 +0530

    Basic React App using vite

---
+++ React/basic-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx"
+
+
+
+function App() {
+
+  return (
+    <div>
+      <ProductTab/>
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ React/basic-react-app/src/ProductTab.jsx
@@ -0,0 +1,12 @@
+import Product from "./Product.jsx";
+
+function ProductTab(){
+    return (
+        <div>
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    );
+}
+export default ProductTab;
