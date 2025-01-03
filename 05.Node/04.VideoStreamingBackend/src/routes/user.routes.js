7b94f6dd4193948759d3c7dc76fd9266dd0ed908
Tauqueer Alam <tauqueeralam42@gmail.com>
2025-01-03 11:29:53 +0530

    Add user authentication routes for login, token refresh, password change, and profile management

--- 05.Node/04.VideoStreamingBackend/src/routes/user.routes.js
+++ 05.Node/04.VideoStreamingBackend/src/routes/user.routes.js
@@ -1,6 +1,18 @@
 import {Router} from 'express';
 
-import { registerUser, logoutUser } from '../controllers/user.controllers.js';
+import { 
+    registerUser, 
+    logoutUser, 
+    loginUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getWatchHistory, 
+} from '../controllers/user.controllers.js';
 import { upload } from '../middlewares/multer.middlewares.js';
 import auth from '../middlewares/auth.middlewares.js';
 
@@ -8,6 +20,8 @@ import auth from '../middlewares/auth.middlewares.js';
 
 const router = Router();
 
+
+// Register a new user
 router.route('/register').post(
     upload.fields([
         {
@@ -21,7 +35,22 @@ router.route('/register').post(
     ]),
     registerUser
 );
+router.route('/login').post(loginUser);
+router.route("/refresh-token").post( refreshAccessToken);
+
+
 
 router.route('/logout').post(auth, logoutUser);
+router.route("/change-password").post(auth, changeCurrentPassword);
+router.route("/current-user").get(auth, getCurrentUser);
+router.route("/c/:username").get(auth, getUserChannelProfile);
+router.route("/update-account").patch(auth,updateAccountDetails);
+router.route("/history").get(auth, getWatchHistory);
+
+
+router.route("/avatar").patch(auth, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(auth, upload.single("coverImage"), updateUserCoverImage);
+
+
 
 export default router
